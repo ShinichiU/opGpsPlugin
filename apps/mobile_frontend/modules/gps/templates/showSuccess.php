0cb80747f6c8396d06a1fa4bb38552493f127adc
Shinichi Urabe <urabe@tejimaya.com>
2009-12-06 18:27:45 +0900

    add mobile map

--- apps/mobile_frontend/modules/gps/templates/showSuccess.php
+++ apps/mobile_frontend/modules/gps/templates/showSuccess.php
@@ -1,3 +1,4 @@
+<?php use_helper('opGps') ?>
 <?php op_mobile_page_title(__('Foot path of %1%', array('%1%' => $member->getName()))) ?>
 
 <?php echo op_within_page_link() ?>
@@ -12,7 +13,8 @@
 
 <?php $apikey = sfConfig::get('google_maps_api_key') ?>
 <?php if (!empty($apikey)): ?>
-<img src="http://maps.google.com/staticmap?center=<?php echo$memberGpsPosition->getLat() ?>,<?php echo$memberGpsPosition->getLon() ?>&zoom=15&size=220x220&maptype=mobile&markers=<?php echo$memberGpsPosition->getLat() ?>,<?php echo$memberGpsPosition->getLon() ?>,blueb&key=<?php echo $apikey ?>" />
+<?php $params = op_calc_gcs_to_WGS84($memberGpsPosition) ?>
+<img src="http://maps.google.com/staticmap?center=<?php echo $params['lat'] ?>,<?php echo $params['lon'] ?>&zoom=15&size=400x400&maptype=mobile&markers=<?php echo $params['lat'] ?>,<?php echo $params['lon'] ?>,blueb&key=<?php echo $apikey ?>" />
 <?php else: ?>
 <?php echo __('empty google map api key.') ?>
 <?php endif; ?>
