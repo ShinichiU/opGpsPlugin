9572f8bb5b127e54f8bc70be74ef3590c89e1a5e
Shinichi Urabe <urabe@tejimaya.com>
2009-12-07 00:39:33 +0900

    fixed bug use undefined variable.

--- apps/pc_frontend/modules/gps/templates/_infomationNowPosition.php
+++ apps/pc_frontend/modules/gps/templates/_infomationNowPosition.php
@@ -1,7 +1,8 @@
+<?php use_helper('opGps') ?>
 <?php if (count($recentGpsList)): ?>
 <ul>
 <?php foreach ($recentGpsList as $_gps): ?>
-<li><?php echo op_gps_link_to_show($position, true) ?> (<?php echo $_gps->getCarrier() ?>)</li>
+<li><?php echo op_gps_link_to_show($_gps, true) ?> (<?php echo $_gps->getCarrier() ?>)</li>
 <?php endforeach; ?>
 </ul>
 <?php endif; ?>
